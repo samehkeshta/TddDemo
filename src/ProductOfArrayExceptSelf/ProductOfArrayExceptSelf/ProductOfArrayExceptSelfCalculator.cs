1c3161145907341a932f8bf4b2646a0607631f23
sameh <sameh.keshta@legate.com.sa>
2022-01-23 00:20:29 +0200

    solve Product of Array Except Self problem

---
+++ src/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf/ProductOfArrayExceptSelfCalculator.cs
@@ -0,0 +1,34 @@
+﻿namespace ProductOfArrayExceptSelf
+{
+    /// <summary>
+    /// https://leetcode.com/problems/product-of-array-except-self/
+    /// </summary>
+    public class ProductOfArrayExceptSelfCalculator
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            if (nums.Length < 2) throw new Exception("Array Length should be greater than or equal 2.");
+
+            var answers = new int[nums.Length];
+            var prefixProduct = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                answers[i] = prefixProduct;
+
+                prefixProduct *= nums[i];
+            }
+
+            var suffixProduct = 1;
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                answers[i] *= suffixProduct;
+
+                suffixProduct *= nums[i];
+            }
+
+            return answers;
+        }
+    }
+}
---
+++ src/ProductOfArrayExceptSelf/ProductOfArrayExceptSelfUnitTests/ProductOfArrayExceptSelfCalculatorTests.cs
@@ -0,0 +1,54 @@
+using ProductOfArrayExceptSelf;
+using System;
+using Xunit;
+
+namespace ProductOfArrayExceptSelfUnitTests
+{
+    public class ProductOfArrayExceptSelfCalculatorTests
+    {
+        private ProductOfArrayExceptSelfCalculator _productOfArrayExceptSelfCalculator;
+
+        public ProductOfArrayExceptSelfCalculatorTests()
+        {
+            _productOfArrayExceptSelfCalculator = new ProductOfArrayExceptSelfCalculator();
+        }
+
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(new int[] { 5 })]
+        public void ArrayHaveLessThan2Items_ThrowsException(int[] nums)
+        {
+            Assert.Throws<Exception>(() => _productOfArrayExceptSelfCalculator.ProductExceptSelf(nums));
+        }
+
+        [Fact]
+        public void ArrayHave2Items_ReturnsTheArrayReverse()
+        {
+            var nums = new int[] { 1, 2 };
+
+            var result = _productOfArrayExceptSelfCalculator.ProductExceptSelf(nums);
+
+            Assert.Equal(new int[] { 2, 1 }, result);
+        }
+
+        [Fact]
+        public void ArrayHavePositiveItems_ReturnsCorrectArray()
+        {
+            var nums = new int[] { 1, 2, 3, 4 };
+
+            var result = _productOfArrayExceptSelfCalculator.ProductExceptSelf(nums);
+
+            Assert.Equal(new int[] { 24, 12, 8, 6 }, result);
+        }
+
+        [Fact]
+        public void ArrayHaveNegativeItems_ReturnsCorrectArray()
+        {
+            var nums = new int[] { -1, 1, 0, -3, 3 };
+
+            var result = _productOfArrayExceptSelfCalculator.ProductExceptSelf(nums);
+
+            Assert.Equal(new int[] { 0, 0, 9, 0, 0 }, result);
+        }
+    }
+}
